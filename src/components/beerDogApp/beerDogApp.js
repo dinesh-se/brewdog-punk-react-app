c719de7a8b726c0670e9e81c6f3e1ecf906d02c5
Dinesh Haribabu <dinesh.be.gct@gmail.com>
2019-07-22 19:04:04 +0100

    Fixed a typo

--- src/components/beerDogApp/beerDogApp.js
+++ src/components/beerDogApp/beerDogApp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../header/Header';
+import Header from '../header/header';
 import BeerBanner from '../beerBanner/beerBanner';
 import SearchForm from '../searchForm/searchForm';
 import './beerDogApp.scss';
