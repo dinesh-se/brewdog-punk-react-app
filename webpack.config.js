1cac3d9af0117f920ef16414f6ab980988077636
Dinesh Haribabu <dinesh.be.gct@gmail.com>
2019-07-22 18:51:57 +0100

    Setup production build and express server

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,47 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'scripts/bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: ['style-loader', 'css-loader', 'sass-loader']
-    }],
-  },
-  devtool: 'eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public')
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'scripts/bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      }],
+    },
+    plugins: [
+      new ExtractTextPlugin("styles/styles.css"),
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public')
+    }
   }
-}
+};
